3cee41adef9d43020073ea4508c2ed9ab8076ef0
Dhruv-Varshney-Developer <f20200865@pilani.bits-pilani.ac.in>
2024-09-26 18:46:02 +0530

    header updated.

--- src/components/header.js
+++ src/components/header.js
@@ -1,32 +1,37 @@
 import React from "react";
-import { AppBar, Toolbar, Typography, Button } from "@mui/material";
+import { AppBar, Toolbar, Button } from "@mui/material";
 import { Link } from "react-router-dom";
 import EtherliteLogo from "../assets/etherlite-logo.png";
 
 function Header() {
   return (
     <AppBar position="fixed">
-      <Toolbar>
+      <Toolbar
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
         <img
           src={EtherliteLogo}
           alt="Etherlite Logo"
-          style={{ height: 40, marginRight: 16 }}
+          style={{ height: 100, marginRight: 16 }}
         />
-        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-          Etherlite
-        </Typography>
-        <Button color="inherit" component={Link} to="/">
-          Portfolio
-        </Button>
-        <Button color="inherit" component={Link} to="/nft">
-          NFT
-        </Button>
-        <Button color="inherit" component={Link} to="/trade">
-          Trade
-        </Button>
-        <Button color="inherit" component={Link} to="/transactions">
-          Transactions
-        </Button>
+        <div style={{ display: "flex", gap: "16px" }}>
+          <Button color="inherit" component={Link} to="/">
+            Portfolio
+          </Button>
+          <Button color="inherit" component={Link} to="/nft">
+            NFT
+          </Button>
+          <Button color="inherit" component={Link} to="/trade">
+            Trade
+          </Button>
+          <Button color="inherit" component={Link} to="/transactions">
+            Transactions
+          </Button>
+        </div>
       </Toolbar>
     </AppBar>
   );
