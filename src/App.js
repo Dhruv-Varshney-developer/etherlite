cb2e5b08f1d4bb7ac8687af13dd354abc61bca7b
Dhruv-Varshney-Developer <f20200865@pilani.bits-pilani.ac.in>
2024-09-26 16:13:09 +0530

    padding added between header and screens.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { ThemeProvider, createTheme, CssBaseline } from "@mui/material";
+import { ThemeProvider, createTheme, CssBaseline, Box } from "@mui/material";
 import Header from "./components/header";
 import Portfolio from "./screens/portfolio";
 import NFT from "./screens/nft";
@@ -22,15 +22,25 @@ function App() {
     <ThemeProvider theme={darkTheme}>
       <CssBaseline />
       <Router>
-        <Header />
-        
-        <Routes>
-          <Route exact path="/" element={<Portfolio />} />
-          <Route path="/nft" element={<NFT />} />
-          <Route path="/trade" element={<Trade />} />
-          <Route path="/transactions" element={<Transactions />} />
-          <Route path="/settings" element={<Settings />} />
-        </Routes>
+        <Box sx={{ display: "flex", flexDirection: "column", height: "100vh" }}>
+          {/* Header */}
+          <Box>
+            <Header />
+          </Box>
+
+          {/* Content */}
+          <Box sx={{ flex: 1, display: "flex", marginTop: 15 }}>
+            <Box sx={{ flexGrow: 1, padding: 2 }}>
+              <Routes>
+                <Route path="/" element={<Portfolio />} />
+                <Route path="/nft" element={<NFT />} />
+                <Route path="/trade" element={<Trade />} />
+                <Route path="/transactions" element={<Transactions />} />
+                <Route path="/settings" element={<Settings />} />
+              </Routes>
+            </Box>
+          </Box>
+        </Box>
       </Router>
     </ThemeProvider>
   );
